ba6cb2661891dce1d06ee9d3e70249d48e6489e8
nathansandford <nathan_sandford@berkeley.edu>
2019-07-31 13:38:39 +0100

    file request requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 setup(
     name='Chem-I-Calc',
     version='0.1.dev0',
-    packages=['chemicalc'],
-    package_data={'chemicalc': ['data/*.h5']},
+    packages=['chemicalc', 'chemicalc_app'],
+    scripts=['chemicalc_app/run_chemicalc'],
     author='Nathan Sandford',
     author_email='nathan_sandford@berkeley.edu',
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=[
+        'tqdm',
+        'requests',
         'dash',
         'flask',
         'mendeleev',
