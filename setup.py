8cbeaa29bcb1dca7b033e65e8db16e4eaf64e218
nathansandford <nathan_sandford@berkeley.edu>
2019-09-16 16:01:01 -0700

    update versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Chem-I-Calc',
-    version='0.1.3dev4',
+    version='0.1.4',
     url='https://github.com/NathanSandford/Chem-I-Calc',
     packages=['chemicalc', 'chemicalc_app'],
     scripts=['chemicalc_app/run_chemicalc'],
