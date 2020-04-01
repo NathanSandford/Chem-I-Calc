eeb7a3ca9335e773c42280f9fba971313fe93005
nathansandford <nathan_sandford@berkeley.edu>
2020-04-01 10:06:53 -0700

    tests for generate_wavelength_template

--- chemicalc/tests/test_utils.py
+++ chemicalc/tests/test_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import chemicalc.reference_spectra as ref
 from chemicalc import utils as u
 
+
 def test_filter_file_exist():
     assert u.filter_file.exists()
 
@@ -21,7 +22,7 @@ def test_find_nearest():
     assert u.find_nearest(test_array, 50) == 50
     assert u.find_nearest(test_list, 50) == 50
     with pytest.raises(TypeError):
-        u.find_nearest('str')
+        u.find_nearest("str")
         u.find_nearest(range(100))
 
 
@@ -31,12 +32,26 @@ def test_find_nearest_idx():
     assert u.find_nearest_idx(test_array, 50) == int(50)
     assert u.find_nearest_idx(test_list, 50) == int(50)
     with pytest.raises(TypeError):
-        u.find_nearest_idx('str')
+        u.find_nearest_idx("str")
         u.find_nearest_idx(range(100))
 
 
 def test_gen_wave_template():
-    pass
+    wave = u.generate_wavelength_template(6500, 9000, 6500, 4, False)
+    log_wave_diff = np.diff(np.log10(wave))
+    assert wave.shape == (8463,)
+    assert np.all(u.generate_wavelength_template(6500, 9000, 6500, 4, True) == wave[:-1])
+    assert np.std(log_wave_diff) < 1e-10
+    with pytest.raises(TypeError):
+        u.generate_wavelength_template('str', 6000, 2000, 3, False)
+        u.generate_wavelength_template(5000, 'str', 2000, 3, False)
+        u.generate_wavelength_template(5000, 6000, 'str', 3, False)
+        u.generate_wavelength_template(5000, 6000, 2000, 'str', False)
+    with pytest.raises(ValueError):
+        u.generate_wavelength_template(-1, 6000, 2000, 3, False)
+        u.generate_wavelength_template(5000, 6000, -1, 3, False)
+        u.generate_wavelength_template(5000, 6000, 2000, -1, False)
+        u.generate_wavelength_template(6000, 5000, 2000, 3, False)
 
 
 def test_convolve_spec():
@@ -63,10 +78,19 @@ def test_kpc_to_mu():
     assert u.kpc_to_mu(0) == -np.inf
     assert np.all(u.kpc_to_mu(ten_pc_in_kpc * np.ones(10)) == np.zeros(10))
     assert np.all(u.kpc_to_mu([ten_pc_in_kpc] * 10) == np.zeros(10))
+    with pytest.raises(TypeError):
+        u.kpc_to_mu("str")
 
 
 def test_mu_to_kpc():
     ten_pc_in_kpc = 10 * 1e-3
     assert u.mu_to_kpc(u.kpc_to_mu(ten_pc_in_kpc)) == ten_pc_in_kpc
-    assert np.all(u.mu_to_kpc(u.kpc_to_mu(ten_pc_in_kpc * np.ones(10))) == ten_pc_in_kpc * np.ones(10))
-    assert np.all(u.mu_to_kpc(u.kpc_to_mu([ten_pc_in_kpc] * 10)) == ten_pc_in_kpc * np.ones(10))
+    assert np.all(
+        u.mu_to_kpc(u.kpc_to_mu(ten_pc_in_kpc * np.ones(10)))
+        == ten_pc_in_kpc * np.ones(10)
+    )
+    assert np.all(
+        u.mu_to_kpc(u.kpc_to_mu([ten_pc_in_kpc] * 10)) == ten_pc_in_kpc * np.ones(10)
+    )
+    with pytest.raises(TypeError):
+        u.mu_to_kpc("str")
