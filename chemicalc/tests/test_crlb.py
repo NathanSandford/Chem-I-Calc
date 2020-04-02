d3290a91d4bf18b22f6d6f4f49c35d184d2d6ca1
nathansandford <nathan_sandford@berkeley.edu>
2020-04-02 15:46:32 -0700

    init tests for crlb.py

---
+++ chemicalc/tests/test_crlb.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+from pathlib import Path
+
+import chemicalc.reference_spectra as ref
+from chemicalc import crlb
+
+test_file_dir = Path(os.path.dirname(__file__)).joinpath("files")
+
+
+def test_calc_crlb():
+    # ToDo: Unit Tests
+    # ToDo: Catch TypeErrors
+    pass
+
+
+def test_sort_crlb():
+    # ToDo: Unit Tests
+    # ToDo: Catch TypeErrors
+    pass
