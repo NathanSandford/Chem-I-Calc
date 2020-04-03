0cb5bde5268b938c1c8b38769cd5e8c41b3ac8bc
nathansandford <nathan_sandford@berkeley.edu>
2020-04-03 15:21:51 -0700

    move download functions to file_mgmt.py

--- chemicalc/utils.py
+++ chemicalc/utils.py
@@ -1,18 +1,11 @@
-import os
-from typing import Any, List, Tuple, Dict, Union, Optional, cast
+from typing import Any, List, Union, Optional, cast
 from warnings import warn
-from pathlib import Path
-import requests
-from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from numpy.fft import rfftfreq
 from scipy.interpolate import interp1d
-from chemicalc import exception as e
 
 
-data_dir: Path = Path(os.path.dirname(__file__)).joinpath("data")
-data_dir.mkdir(exist_ok=True)
 alpha_el: List[str] = ["O", "Ne", "Mg", "Si", "S", "Ar", "Ca", "Ti"]
 
 
@@ -300,76 +293,3 @@ def mu_to_kpc(
         mu = np.array(mu)
         mu = cast(np.ndarray, mu)
     return 1 / (1e3 / 10) * 10 ** (mu / 5)
-
-
-def download_package_files(id, destination):
-    def get_confirm_token(response):
-        for key, value in response.cookies.items():
-            if key.startswith("download_warning"):
-                return value
-
-        return None
-
-    def save_response_content(response, destination):
-        chunk_size = 32768
-
-        with open(destination, "wb") as f:
-            for chunk in tqdm(response.iter_content(chunk_size)):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
-
-    url = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(url, params={"id": id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {"id": id, "confirm": token}
-        response = session.get(url, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-
-def download_all_files(overwrite=True):
-    from chemicalc.reference_spectra import label_file, label_id
-    from chemicalc.reference_spectra import (
-        precomputed_res,
-        precomputed_ref_id,
-        precomputed_cont_id,
-    )
-
-    if label_file.exists() and not overwrite:
-        print(f"{label_file} exists")
-    else:
-        print(f"Downloading {label_file}.h5")
-        download_package_files(id=label_id, destination=label_file)
-
-    for res in precomputed_res:
-        resolution = precomputed_res[res]
-        reference_file = data_dir.joinpath(f"reference_spectra_{resolution:06}.h5")
-        reference_id = precomputed_ref_id[res]
-        if reference_file.exists() and not overwrite:
-            print(f"{reference_file} exists")
-        else:
-            print(f"Downloading {reference_file}")
-            download_package_files(id=reference_id, destination=reference_file)
-
-    for res in precomputed_res:
-        resolution = precomputed_res[res]
-        continuum_file = data_dir.joinpath(f"reference_continuum_{resolution:06}.h5")
-        continuum_id = precomputed_cont_id[res]
-        if continuum_file.exists() and not overwrite:
-            print(f"{continuum_file} exists")
-        else:
-            print(f"Downloading {continuum_file}")
-            download_package_files(id=continuum_id, destination=continuum_file)
-
-
-def check_label_format():
-    raise NotImplementedError("Coming soon!")
-
-
-def check_spec_format():
-    raise NotImplementedError("Coming soon!")
