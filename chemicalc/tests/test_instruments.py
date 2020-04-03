c9677676a2a0259242857f8b42670df0b8ab575a
nathansandford <nathan_sandford@berkeley.edu>
2020-04-03 12:57:33 -0700

    init tests for instruments.py

---
+++ chemicalc/tests/test_instruments.py
@@ -0,0 +1,56 @@
+import pytest
+
+import chemicalc.instruments as inst
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_InstConfig_init():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_InstConfig_setwave():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_InstConfig_resetwave():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_InstConfig_setsnr():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_InstConfig_summary():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_InstConfig_len():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_AllInst_init():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_AllInst_listspec():
+    # ToDo: Implement Tests
+    pass
+
+
+@pytest.mark.skip(reason="Test not implemented")
+def test_AllInst_getspec():
+    # ToDo: Implement Tests
+    pass
