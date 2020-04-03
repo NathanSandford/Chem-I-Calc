776098c2546166666934a7c00fe00fcbd31f2e05
nathansandford <nathan_sandford@berkeley.edu>
2020-04-03 12:57:45 -0700

    ToDo's

--- chemicalc/tests/test_reference_spectra.py
+++ chemicalc/tests/test_reference_spectra.py
@@ -15,41 +15,48 @@ def test_alpha_el():
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_init():
+    # ToDo: Implement Tests
     star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
 
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_addrv():
+    # ToDo: Implement Tests
     #star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
 
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_convolve():
+    # ToDo: Implement Tests
     #star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
 
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_calcgrad():
+    # ToDo: Implement Tests
     #star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
 
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_zerograd():
+    # ToDo: Implement Tests
     #star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
 
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_getnames():
+    # ToDo: Implement Tests
     #star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
 
 
 @pytest.mark.skip(reason="Test not implemented")
 def test_RefSpec_reset():
+    # ToDo: Implement Tests
     #star = ref.ReferenceSpectra(reference="RGB_m1.5")
     pass
