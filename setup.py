39d2db318cf368893b6e6fd7eda8f03d63e351eb
nathansandford <nathan_sandford@berkeley.edu>
2020-04-03 13:36:32 -0700

    packaging updates

--- setup.py
+++ setup.py
@@ -1,27 +1,123 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Package meta-data.
+NAME = "Chem-I-Calc"
+DESCRIPTION = "Chemical information calculator for resolved stellar spectroscopy"
+URL = "https://github.com/NathanSandford/Chem-I-Calc"
+EMAIL = "nathan_sandford@berkeley.edu"
+AUTHOR = "Nathan Sandford"
+REQUIRES_PYTHON = ">=3.5.0"
+VERSION = "0.2.0dev"
+
+
+def get_requirements():
+    """ Get the requirements from a system file.  """
+    name = 'requirements.txt'
+
+    requirements_file = os.path.join(here, name)
+    install_requires = [line.strip().replace('==', '>=') for line in open(requirements_file)
+                        if not line.strip().startswith('#') and line.strip() != '']
+    return install_requires
+
+
+REQUIRED = get_requirements()
+EXTRAS = {}
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='Chem-I-Calc',
-    version='0.1.4',
-    url='https://github.com/NathanSandford/Chem-I-Calc',
-    packages=['chemicalc', 'chemicalc_app'],
-    scripts=['chemicalc_app/run_chemicalc'],
-    author='Nathan Sandford',
-    author_email='nathan_sandford@berkeley.edu',
-    license='MIT',
-    long_description=open('README.md').read(),
-    install_requires=[
-        'tqdm',
-        'requests',
-        'dash',
-        'flask',
-        'mendeleev',
-        'matplotlib',
-        'plotly',
-        'scipy',
-        'mechanicalsoup',
-        'pandas',
-        'numpy',
-        'tables',
+    name=NAME,
+    version=about["__version__"],
+    license="MIT",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    tests_require=['pytest'],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Astronomy",
     ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand,},
 )
