16cb9747ca71a8ed0c7eb31967412f7f6c8338fd
nathansandford <nathan_sandford@berkeley.edu>
2020-04-10 16:19:25 -0700

    deserialization for scrubbed bokeh data

--- chemicalc/utils.py
+++ chemicalc/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from numpy.fft import rfftfreq
 from scipy.interpolate import interp1d
+import base64
 
 
 alpha_el: List[str] = ["O", "Ne", "Mg", "Si", "S", "Ar", "Ca", "Ti"]
@@ -11,8 +12,9 @@ alpha_el: List[str] = ["O", "Ne", "Mg", "Si", "S", "Ar", "Ca", "Ti"]
 
 def find_nearest(array: Union[List[float], np.ndarray], value: float) -> float:
     """
-    ToDo: DocString
-    :param Union[List[float], np.ndarray] array: list or array of floats to search
+    Find the nearest value in an array
+
+    :param Union[List[float],np.ndarray] array: list or array of floats to search
     :param float value: value that you wish to find
     :return float: entry in array that is nearest to value
     """
@@ -27,7 +29,8 @@ def find_nearest(array: Union[List[float], np.ndarray], value: float) -> float:
 
 def find_nearest_idx(array: Union[List[float], np.ndarray], value: float) -> int:
     """
-    ToDo: DocString
+    Find the index of the nearest value in an array
+
     :param Union[List[float], np.ndarray] array: list or array of floats to search
     :param float value: value that you wish to find
     :return int: index of entry in array that is nearest to value
@@ -50,6 +53,7 @@ def generate_wavelength_template(
 ) -> np.ndarray:
     """
     Generate wavelength array with fixed resolution and wavelength sampling.
+
     :param float start_wavelength: minimum wavelength of spectra to include
     :param float end_wavelength: maximum wavelength of spectra to include
     :param float resolution: resolving power of instrument (R = lambda / delta lambda)
@@ -81,6 +85,47 @@ def generate_wavelength_template(
     return wavelength_template
 
 
+def doppler_shift(
+    wave: np.ndarray, spec: np.ndarray, rv: float, bounds_warning: bool = True
+) -> Union[np.ndarray, Any]:
+    """
+    Apply doppler shift to spectra and resample onto original wavelength grid
+
+    Warning: This function does not gracefully handle spectral regions that are shifted into the wavelength range. Presently, these are set to np.nan.
+
+    :param np.ndarray wave: input wavelength array
+    :param np.ndarray spec: input spectra array
+    :param float rv: Radial Velocity (km/s)
+    :param bool bounds_warning: warn about boundary issues?
+    :return Union[np.ndarray,Any]: Doppler shifted spectra array
+    """
+    if not all(isinstance(i, np.ndarray) for i in [wave, spec]):
+        raise TypeError("wave and spec must be np.ndarray")
+    if not isinstance(rv, (int, float)):
+        raise TypeError("rv must be an int or float")
+    if not np.all(np.diff(wave) > 0):
+        raise ValueError("wave must be sorted")
+    if rv < 0:
+        raise ValueError("rv must be > 0")
+    c = 2.99792458e5  # km/s
+    doppler_factor = np.sqrt((1 - rv / c) / (1 + rv / c))
+    new_wavelength = wave * doppler_factor
+    shifted_spec = np.interp(new_wavelength, wave, spec)
+    shifted_spec[(wave < new_wavelength.min()) | (wave > new_wavelength.max())] = np.nan
+    if bounds_warning:
+        if rv > 0:
+            warn(
+                f"Spectra for wavelengths below {new_wavelength.min()} are undefined; set to np.nan",
+                UserWarning,
+            )
+        if rv < 0:
+            warn(
+                f"Spectra for wavelengths above {new_wavelength.max()} are undefined; set to np.nan",
+                UserWarning,
+            )
+    return shifted_spec
+
+
 def convolve_spec(
     wave: np.ndarray,
     spec: np.ndarray,
@@ -90,12 +135,13 @@ def convolve_spec(
 ) -> Union[np.ndarray, Any]:
     """
     Convolves spectrum to lower resolution and samples onto a new wavelength grid
+
     :param np.ndarray wave: input wavelength array
     :param np.ndarray spec: input spectra array (may be 1 or 2 dimensional)
     :param float resolution: Resolving power to convolve down to (R = lambda / delta lambda)
     :param np.ndarray outwave: wavelength grid to sample onto
     :param Optional[float] res_in: Resolving power of input spectra
-    :return Union[np.ndarray, Any]: convolved spectra array
+    :return Union[np.ndarray,Any]: convolved spectra array
     """
     if not all(isinstance(i, np.ndarray) for i in [wave, spec, outwave]):
         raise TypeError("wave, spec, and outwave must be np.ndarray")
@@ -167,46 +213,6 @@ def convolve_spec(
     return fspec(outwave)
 
 
-def doppler_shift(
-    wave: np.ndarray, spec: np.ndarray, rv: float, bounds_warning: bool = True
-) -> Union[np.ndarray, Any]:
-    """
-    ToDo: Unit Tests
-    ToDo: Fix Boundary Issues
-    Apply doppler shift to spectra and resample onto original wavelength grid
-    :param np.ndarray wave: input wavelength array
-    :param np.ndarray spec: input spectra array
-    :param float rv: Radial Velocity (km/s)
-    :param bool bounds_warning: warn about boundary issues?
-    :return Union[np.ndarray, Any]: Doppler shifted spectra array
-    """
-    if not all(isinstance(i, np.ndarray) for i in [wave, spec]):
-        raise TypeError("wave and spec must be np.ndarray")
-    if not isinstance(rv, (int, float)):
-        raise TypeError("rv must be an int or float")
-    if not np.all(np.diff(wave) > 0):
-        raise ValueError("wave must be sorted")
-    if rv < 0:
-        raise ValueError("rv must be > 0")
-    c = 2.99792458e5  # km/s
-    doppler_factor = np.sqrt((1 - rv / c) / (1 + rv / c))
-    new_wavelength = wave * doppler_factor
-    shifted_spec = np.interp(new_wavelength, wave, spec)
-    shifted_spec[(wave < new_wavelength.min()) | (wave > new_wavelength.max())] = np.nan
-    if bounds_warning:
-        if rv > 0:
-            warn(
-                f"Spectra for wavelengths below {new_wavelength.min()} are undefined; set to np.nan",
-                UserWarning,
-            )
-        if rv < 0:
-            warn(
-                f"Spectra for wavelengths above {new_wavelength.max()} are undefined; set to np.nan",
-                UserWarning,
-            )
-    return shifted_spec
-
-
 def calc_gradient(
     spectra: np.ndarray,
     labels: pd.DataFrame,
@@ -214,8 +220,8 @@ def calc_gradient(
     ref_included: bool = True,
 ) -> pd.DataFrame:
     """
-    ToDo: Unit Tests
     Calculates partial derivatives of spectra wrt to each label
+
     :param np.ndarray spectra: input spectra array
     :param pd.DataFrame labels: input label array
     :param bool symmetric: If true, calculates symmetric gradient about reference
@@ -271,7 +277,8 @@ def kpc_to_mu(
 ) -> Union[np.float64, np.ndarray, Any]:
     """
     Converts kpc to distance modulus
-    :param Union[float, List[float], np.ndarray] d: distance in kpc
+
+    :param Union[float,List[float],np.ndarray] d: distance in kpc
     :return Union[np.float64, np.ndarray, Any]: distance modulus
     """
     if not isinstance(d, (int, float, np.ndarray, list)):
@@ -289,7 +296,8 @@ def mu_to_kpc(
 ) -> Union[float, np.float64, np.ndarray]:
     """
     Converts distance modulus to kpc
-    :param Union[float, List[float], np.ndarray] mu: distance modulus
+
+    :param Union[float,List[float],np.ndarray] mu: distance modulus
     :return Union[float, np.float64, np.ndarray]: distance in kpc
     """
     if not isinstance(mu, (int, float, np.ndarray, list)):
@@ -298,3 +306,18 @@ def mu_to_kpc(
         mu = np.array(mu)
         mu = cast(np.ndarray, mu)
     return 1 / (1e3 / 10) * 10 ** (mu / 5)
+
+
+def decode_base64_dict(data):
+    ''' Decode a base64 encoded array into a NumPy array. Lifted from bokeh.serialization
+    Args:
+        data (dict) : encoded array data to decode
+    Data should have the format encoded by :func:`encode_base64_dict`.
+    Returns:
+        np.ndarray
+    '''
+    b64 = base64.b64decode(data['__ndarray__'])
+    array = np.copy(np.frombuffer(b64, dtype=data['dtype']))
+    if len(data['shape']) > 1:
+        array = array.reshape(data['shape'])
+    return array
