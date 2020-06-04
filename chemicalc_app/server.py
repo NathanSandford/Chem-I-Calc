dc59cd0c01d5c6a27d96a2533eb233c72ce6ceb8
nathansandford <nathan_sandford@berkeley.edu>
2020-06-04 09:52:50 -0700

    remove app files from github

--- chemicalc_app/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from dash import Dash
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-server = Flask('chemicalc_app')
-app = Dash(server=server, external_stylesheets=external_stylesheets)
-
-app.config['suppress_callback_exceptions'] = False
